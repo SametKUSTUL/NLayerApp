898882161851d065e6cfa27c00bfb648f821d26d
Samet KUSTUL <47284751+SametKUSTUL@users.noreply.github.com>
2022-06-07 00:00:19 +0300

    Tüm projelerin nullable özelliği kapatıldı. Sağ tık -> Properties->nullable enable/disable

--- NLayer.Core/Product.cs
+++ NLayer.Core/Product.cs
@@ -14,7 +14,6 @@ namespace NLayer.Core
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-
-        public ProductFeature ProductFeature { get; set; }
+        public ProductFeature ProductFeature { get; set; } // ? demek null deger alabilir demek
     }
 }

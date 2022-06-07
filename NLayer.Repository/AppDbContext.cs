cb14be170ada8348b6b98aa08cdd74cf3634ce18
Samet KUSTUL <47284751+SametKUSTUL@users.noreply.github.com>
2022-06-07 14:04:34 +0300

    EntityConfigurations

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -18,5 +18,11 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

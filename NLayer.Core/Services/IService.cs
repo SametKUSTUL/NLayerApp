faf7bd61780d49e05b7fc14ea18cdafcc45c96bd
Samet KUSTUL <47284751+SametKUSTUL@users.noreply.github.com>
2022-06-07 21:55:26 +0300

    Services

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -13,7 +13,7 @@ namespace NLayer.Core.Services
         Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity); 
         Task RemoveAsync(T entity); 

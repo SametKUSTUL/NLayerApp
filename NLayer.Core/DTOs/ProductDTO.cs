0479f069f2e774ec7b9ec0b83f9e8b773e731e69
Samet KUSTUL <47284751+SametKUSTUL@users.noreply.github.com>
2022-06-07 22:26:42 +0300

    AutoMapper Eklendi

--- NLayer.Core/DTOs/ProductDTO.cs
+++ NLayer.Core/DTOs/ProductDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core.DTOs
 {
-    public class ProductDTOs:BaseDTO
+    public class ProductDTO:BaseDTO
     {
         public string Name { get; set; }
         public int Stock { get; set; }
---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product,ProductDTO>().ReverseMap();
+            CreateMap<Category,CategoryDTO>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDTO>().ReverseMap();
+            CreateMap<ProductUpdateDTO, Product>();
+
+        }
+    }
+}
